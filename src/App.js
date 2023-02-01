7019cae1b170f7fd3a2279aa05385a1a9f9ff517
dopswae98 <ephraimmatarutse@gmail.com>
2023-02-01 06:00:52 -0800

    sdhfjsdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <LoginPage />
     </div>
   );
 }
